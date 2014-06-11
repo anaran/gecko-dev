e5656369a215f346321d9f5c266aeb2f149a31c4
Adrian Aichner <adrian.aichner@gmail.com>
2014-06-11 14:44:12 +0200

    Bug 1023839 - Use js-indent-level in .js, .jsm emacs file variables, standardize on mode: js

--- security/manager/ssl/tests/unit/head_psm.js
+++ security/manager/ssl/tests/unit/head_psm.js
@@ -186,7 +186,7 @@ function clearSessionCache() {
 // Your test script should look something like this:
 /*
 
-// -*- Mode: javascript; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// -*- Mode: js; tab-width: 2; indent-tabs-mode: nil; js-indent-level: 2 -*-
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
